fd6ae9ca61ea209f4f346391e1511bb5b810c745
MartinG1341 <geogmartin130@gmail.com>
2026-08-20 16:37:58 +0300

    Car-add form

--- src/pages/AddCar/AddCar.jsx
+++ src/pages/AddCar/AddCar.jsx
@@ -9,6 +9,26 @@ export default function AddCar() {
             Brand:
             <input type="text" />
         </label>
+        <label>
+            Model:
+            <input type="text" />
+        </label>
+        <label>
+            Year:
+            <input type="year" />
+        </label>
+        <label>
+            HP:
+            <input type="number" />
+        </label>
+        <label>
+            Fuel:
+            <input type="text" />
+        </label>
+        <label>
+            Consumption:
+            <input type="number" />
+        </label>                                
     </form>
     </div>       
 )
