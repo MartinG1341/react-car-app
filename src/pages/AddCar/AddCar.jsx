bca90e2b7f1c06807b0de1c33893491fd131b8d3
MartinG1341 <geogmartin130@gmail.com>
2026-08-20 18:14:31 +0300

    Car-add form

--- src/pages/AddCar/AddCar.jsx
+++ src/pages/AddCar/AddCar.jsx
@@ -3,7 +3,18 @@ import { useState } from 'react'
 
 export default function AddCar() {
     const [brand, setBrand] = useState('')
-    const [isPending, setIsPending] = useState('false')
+    const [model, setModel] = useState('')
+    const [year, setYear] = useState('')
+    const [horsepower, setHorsepower] = useState('')
+    const [fuel, setFuel] = useState('')
+    const [consumption, setConsumption] = useState('')
+    const [isPending, setIsPending] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setIsPending(true)
+
+    }
   return (
     <div>
         <h1>Addcar</h1>
@@ -17,39 +28,39 @@ export default function AddCar() {
         <label>
             Model:
             <input type="text"
-            value={brand}
-            onChange={(e) => setBrand(e.target.value)} />
+            value={model}
+            onChange={(e) => setModel(e.target.value)} />
         </label>
         <label>
             Year:
             <input type="number"
-            value={brand}
-            onChange={(e) => setBrand(e.value.target)} 
+            value={year}
+            onChange={(e) => setYear(e.target.value)} 
             />
         </label>
         <label>
             HP:
             <input type="number"
-            value={brand}
-            onChange={(e) => setBrand(e.value.target)}
+            value={horsepower}
+            onChange={(e) => setHorsepower(e.target.value)}
              />
         </label>
         <label>
             Fuel:
             <input type="text"
-            value={brand}
-            onChange={(e) => setBrand(e.value.target)}
+            value={fuel}
+            onChange={(e) => setFuel(e.target.value)}
              />
         </label>
         <label>
             Consumption:
             <input type="number"
-            value={brand}
-            onChange={(e) => setBrand(e.value.target)}
+            value={consumption}
+            onChange={(e) => setConsumption(e.target.value)}
             />
         </label>                                
     </form>
-    {!isPending && <button>Add Car</button>}
+    {!isPending && <button onSubmit={handleSubmit}>Add Car</button>}
     {isPending && <button>loading</button>}
     </div>       
 )
