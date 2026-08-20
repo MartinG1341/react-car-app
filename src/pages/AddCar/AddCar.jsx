b7c11e04da2918467fbc6ee83c005a23e156c9d7
MartinG1341 <geogmartin130@gmail.com>
2026-08-20 18:06:15 +0300

    Car-add form

--- src/pages/AddCar/AddCar.jsx
+++ src/pages/AddCar/AddCar.jsx
@@ -2,38 +2,55 @@ import React from 'react'
 import { useState } from 'react'
 
 export default function AddCar() {
-    cosnt [brand, setBrand] = useState('')
-
+    const [brand, setBrand] = useState('')
+    const [isPending, setIsPending] = useState('false')
   return (
     <div>
         <h1>Addcar</h1>
     <form>
         <label>
             Brand:
-            <input type="text" />
+            <input type="text"
+            value={brand}
+            onChange={(e) => setBrand(e.target.value)}/>
         </label>
         <label>
             Model:
-            <input type="text" />
+            <input type="text"
+            value={brand}
+            onChange={(e) => setBrand(e.target.value)} />
         </label>
         <label>
             Year:
-            <input type="number" />
+            <input type="number"
+            value={brand}
+            onChange={(e) => setBrand(e.value.target)} 
+            />
         </label>
         <label>
             HP:
-            <input type="number" />
+            <input type="number"
+            value={brand}
+            onChange={(e) => setBrand(e.value.target)}
+             />
         </label>
         <label>
             Fuel:
-            <input type="text" />
+            <input type="text"
+            value={brand}
+            onChange={(e) => setBrand(e.value.target)}
+             />
         </label>
         <label>
             Consumption:
-            <input type="number" />
+            <input type="number"
+            value={brand}
+            onChange={(e) => setBrand(e.value.target)}
+            />
         </label>                                
     </form>
-    <button>Add Car</button>
+    {!isPending && <button>Add Car</button>}
+    {isPending && <button>loading</button>}
     </div>       
 )
 }
