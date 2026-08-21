348a10e3d9f8b67f38410f556186802bd11ad26a
MartinG1341 <geogmartin130@gmail.com>
2026-08-21 14:43:58 +0300

    Car-add form

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import styles from './Navbar.module.css'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className={styles.navbar}>
+        <h2>Carly</h2>
+        <div className={styles.links}>
+            <Link to='/'>Home</Link>
+            <Link to='/garage'>Garage</Link>
+            <Link to='/add-car'>Add Car</Link>
+        </div>
+    </nav>
+
+
   )
 }
--- src/pages/Garage/Garage.jsx
+++ src/pages/Garage/Garage.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import styles from './Garage.module.css'
+import { useEffect, useState } from 'react'
 
 export default function Garage() {
+  const [cars, setCars] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:3001/cars')
+    .then((res) => res.json)
+    .then((data) => {
+      setCars(data)
+    })
+  }, [])
+
+
   return (
-    <div>Garage</div>
+    <div>
+    <p>Garage</p>
+    <h1>{cars.length} car</h1>
+    </div>
   )
 }
