d01d2f5c300bcc0ef321948f8a15ae6ddde51969
MartinG1341 <geogmartin130@gmail.com>
2026-08-21 19:23:29 +0300

    Car-add form

--- src/pages/Garage/Garage.jsx
+++ src/pages/Garage/Garage.jsx
@@ -12,10 +12,31 @@ export default function Garage() {
     })
   }, [])
 
+  const handleDelete = async (id) => {
+      id.preventDefault()
+      await fetch(`http://localhost:3001/cars/${id}`,
+        {
+          method: 'DELETE'
+        }
+      )
+  }
+
   return (
     <div className={styles.page}>
     <p>Garage</p>
-    <h1>{cars.length === 0 ? "Don't have cars" : `${cars.lenght} ${cars.lenght === 1 ? 'car' : 'cars'}`} </h1>
+    <h1>{cars.length === 0 ? "Don't have cars" : `${cars.length} ${cars.length === 1 ? 'car' : 'cars'}`} </h1>
+    <div className={styles.carsGrid}>
+    {cars.map((car) => (
+      <div className={styles.carCard} key={car.id}>
+        <h2>{car.brand} {car.model}</h2>
+        <p>Year: {car.year}</p>
+        <p>HP: {car.horsepower}</p>
+        <p>Fuel: {car.fuel}</p>
+        <p>Consumption: {car.consumption} L/100km</p>
+        <button className={styles.deleteButton} onClick={() => handleDelete(car.id)}>Delete car</button>
+      </div>
+    ))}
+  </div>
     </div>
   )
 }
